934d71e1b36ae2d3ef81baa5beca5f7ed6957ffc
Maev4l <maeval.nightingale@gmail.com>
2021-05-26 09:33:59 +0200

    Use custom build images

--- README.md
+++ README.md
@@ -22,11 +22,13 @@ ansible-inventory --inventory aws_ec2.yaml --list
 ansible-playbook --inventory aws_ec2.yaml main.yaml
 ```
 
-## Custom Docker image
+## Custom Docker image for Jenkins
 
 ```
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 671123374425.dkr.ecr.eu-central-1.amazonaws.com
 ```
+
+### Jenkins
 
 ```
 docker build -t jenkins:1.0 .
@@ -44,6 +46,34 @@ docker push 671123374425.dkr.ecr.eu-central-1.amazonaws.com/jenkins:1.0
 
 ```
 docker run --name jenkins --rm -p 8080:8080 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password jenkins:1.0
+```
+
+### NodeJS
+
+```
+docker build -t jenkins/nodejs:14 .
+```
+
+```
+docker tag jenkins/nodejs:14 671123374425.dkr.ecr.eu-central-1.amazonaws.com/jenkins/nodejs:14
+```
+
+```
+docker push 671123374425.dkr.ecr.eu-central-1.amazonaws.com/jenkins/nodejs:14
+```
+
+### Python
+
+```
+docker build -t jenkins/python:3.8 .
+```
+
+```
+docker tag jenkins/nodejs:14 671123374425.dkr.ecr.eu-central-1.amazonaws.com/jenkins/python:3.8
+```
+
+```
+docker push 671123374425.dkr.ecr.eu-central-1.amazonaws.com/jenkins/python:3.8
 ```
 
 ## References
--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,5 @@
-resource "aws_ecr_repository" "image_resize_repository" {
+
+resource "aws_ecr_repository" "image_jenkins_repository" {
   name                 = "jenkins"
   image_tag_mutability = "MUTABLE"
 
@@ -7,7 +8,36 @@ resource "aws_ecr_repository" "image_resize_repository" {
   }
 }
 
-resource "aws_ecr_lifecycle_policy" "image_common_repository_lifecycle" {
-  repository = aws_ecr_repository.image_resize_repository.name
+resource "aws_ecr_lifecycle_policy" "image_jenkins_repository_lifecycle" {
+  repository = aws_ecr_repository.image_jenkins_repository.name
   policy     = file("ecr-lifecycle-policy.json")
 }
+
+resource "aws_ecr_repository" "image_jenkins_python_repository" {
+  name                 = "jenkins/python"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "image_jenkins_python_repository_lifecycle" {
+  repository = aws_ecr_repository.image_jenkins_python_repository.name
+  policy     = file("ecr-lifecycle-policy.json")
+}
+
+resource "aws_ecr_repository" "image_jenkins_nodejs_repository" {
+  name                 = "jenkins/nodejs"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "image_jenkins_nodejs_repository_lifecyle" {
+  repository = aws_ecr_repository.image_jenkins_nodejs_repository.name
+  policy     = file("ecr-lifecycle-policy.json")
+}
+
